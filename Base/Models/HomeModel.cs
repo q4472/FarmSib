b14311c6ce6b74bc8b561b85bd72748d672b2f2c
Sokolov <sokolov_ea@farmsib.ru>
2019-02-14 19:17:52 +0300

    q

--- Base/Models/HomeModel.cs
+++ Base/Models/HomeModel.cs
@@ -135,7 +135,8 @@ namespace FarmSib.Base.Models
                                 { name: 'Судебные документы', url: '/Reports/R3' },
                                 { name: 'Обеспечение', url: null, cont: [
                                     { name: 'Долги по клиентам', url: 'Deposits/F0' },
-                                    { name: 'Расчеты по аукционам', url: 'Deposits/F1' } ] } ] },
+                                    { name: 'Расчеты по аукционам', url: 'Deposits/F1' } ] },
+                                { name: 'МДВ1', url: '/MDVReports/F0' } ] },
                             { name: 'Номенклатура', url: null, cont: [
                                 { name: 'Препараты', url: '/Items/Search' },
                                 { name: 'Группы', url: '/Items/Groups' } ] },
